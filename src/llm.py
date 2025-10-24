d043f532cbf4e73f6c537eb6305f8689d21999f4
alematt0504 <alessiomattiace@libero.it>
2025-10-24 20:21:15 +0200

    temperature

--- src/llm.py
+++ src/llm.py
@@ -24,7 +24,7 @@ def get_llm(model_env_value: str):
                 api_key=api_key,
                 model_name=model_name,
                 base_url=base_url,
-                temperature=0
+                temperature=0.1
                 )
 
         elif model_env_value.startswith('gemini'):
